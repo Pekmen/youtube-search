64916868eb1801bf08b0373977985af149960577
Pekmen <milanpekovic89@gmail.com>
2018-04-05 17:02:35 +0200

    skip saving allready saved videos hotfix

--- src/reducers/savedVideos.js
+++ src/reducers/savedVideos.js
@@ -1,5 +1,7 @@
 const savedVideos = (state = [], action) => {
   if (action.type === 'SAVE_VIDEO') {
+    const allreadySaved = state.filter(obj => obj.id === action.payload.id).length > 0;
+    if (allreadySaved) return state;
     return [
       ...state,
       action.payload,
